530333066370034c415ab77f72ff265bbb16478b
Deepanshu Tiwari <ddepu11@gmail.com>
2021-03-24 16:21:47 +0530

    saving value of theme in local storage

--- src/App.js
+++ src/App.js
@@ -2,28 +2,46 @@ import "./App.css";
 import { FaGooglePlay } from "react-icons/fa";
 import { GiFruitBowl, GiScooter, GiHealthNormal } from "react-icons/gi";
 import { AiOutlineClose, AiOutlineBars } from "react-icons/ai";
-import { useRef } from "react";
+import { useRef, useEffect, useState } from "react";
+
+let themeVal = localStorage.getItem("theme");
+if (themeVal) {
+  themeVal = localStorage.getItem("theme");
+} else {
+  themeVal = "light-theme";
+}
+
 function App() {
   const sideBarRef = useRef();
+
+  const [theme, setTheme] = useState(themeVal);
+
   const handleChange = (e) => {
     const classes = document.documentElement.classList;
-    console.log(classes.contains("light-theme"));
     const bodyEl = document.querySelector(".background");
 
     if (classes.contains("light-theme")) {
       document.documentElement.classList.remove("light-theme");
       document.documentElement.classList.add("dark-theme");
       bodyEl.classList.add("body-dark");
+      localStorage.setItem("theme", "dark-theme");
+      setTheme("dark-theme");
     } else {
       document.documentElement.classList.remove("dark-theme");
       document.documentElement.classList.add("light-theme");
       bodyEl.classList.remove("body-dark");
+      localStorage.setItem("theme", "light-theme");
+      setTheme("light-theme");
     }
   };
 
   const handleMenu = () => {
     sideBarRef.current.classList.toggle("show-sidebar");
   };
+
+  useEffect(() => {
+    document.documentElement.classList.add(theme.trim());
+  });
 
   return (
     <div className="container">
@@ -52,7 +70,12 @@ function App() {
         </ul>
 
         <label className="switch">
-          <input type="checkbox" className="toggle" onChange={handleChange} />
+          <input
+            type="checkbox"
+            className="toggle"
+            onChange={handleChange}
+            checked={theme === "dark-theme" ? true : false}
+          />
           <span className="slider round"></span>
         </label>
         <AiOutlineBars className="menu-bar" onClick={handleMenu} />
